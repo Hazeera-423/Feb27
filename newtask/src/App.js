c720c83922b0d28978c6590cfbad7ab87843acf7
Hazerra-423 <hazeera9515@gmail.com>
2025-02-27 13:06:43 +0530

    new task is added

---
+++ newtask/src/App.js
@@ -0,0 +1,17 @@
+//import Form1 from "./Task1/Form";
+
+import NotesManager from "./Task1/addtask";
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello world</h1>
+
+     {/* <Form1/> */}
+     <NotesManager/>
+    </div>
+  );
+}
+
+export default App;
